55071575bcde910f1d136d19925f2aac3116b346
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-04-10 03:20:43 +1000

    Update __init__.py

--- image_manager/__init__.py
+++ image_manager/__init__.py
@@ -1,1 +1,20 @@
-#empty 
+# image_manager/__init__.py
+
+"""
+Image Manager Module Initialization
+
+This module handles:
+- Container image management
+- Building, storing, and retrieving images
+- Import/export of filesystem snapshots as images
+
+Compatible with Termux Python 3.10+.
+"""
+
+from .image_manager_core import ImageManager
+from .image_utils import ImageUtils
+
+__all__ = [
+    "ImageManager",
+    "ImageUtils",
+]
